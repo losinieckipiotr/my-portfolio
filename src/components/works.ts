67caf53dc49a1f84c9c79436a03eb5c264e01e9d
Piotr Łosiniecki <losinieckipiotr@gmail.com>
2023-07-19 22:16:58 +0200

    gallery for mobile and desktop

---
+++ src/components/works.ts
@@ -0,0 +1,11 @@
+export const works = [
+	{ title: 'Pagóry',     href: '/mywork/peaks',     src: 'assets/minatures/1.jpg', alt: '' },
+	{ title: 'Piaseczki',     href: '/mywork/coast',     src: 'assets/minatures/2.jpg', alt: '' },
+	{ title: 'Dziki źwież',  href: '/mywork/wildlife',  src: 'assets/minatures/3.jpg', alt: '' },
+	{ title: 'Parawany', href: '/mywork/seafaring', src: 'assets/minatures/4.jpg', alt: '' },
+	{ title: 'Dowód że ziemia jest płaska',   href: '/mywork/horizon',   src: 'assets/minatures/5.jpg', alt: '' },
+	{ title: 'Rodzinne strony James\'a Bonda', href: '/mywork/landscape', src: 'assets/minatures/6.jpg', alt: '' },
+	{ title: 'Planeta z Interstellar',  href: '/mywork/crystals',  src: 'assets/minatures/7.jpg', alt: '' },
+	{ title: 'Schody do wody',     href: '/mywork/wharf',     src: 'assets/minatures/8.jpg', alt: '' },
+	{ title: 'Łoweczki',      href: '/mywork/dock',      src: 'assets/minatures/9.jpg', alt: '' }
+]
