0ae80d060e70af24247db0427755d9f487625b82
Piotr Łosiniecki <losinieckipiotr@gmail.com>
2023-07-19 23:47:40 +0200

    more links fixed

--- src/components/my/works.ts
+++ src/components/my/works.ts
@@ -1,11 +1,11 @@
 export const works = [
-	{ title: 'Pagóry',     href: '/mywork/peaks',     src: 'assets/minatures/1.jpg', alt: '' },
-	{ title: 'Piaseczki',     href: '/mywork/coast',     src: 'assets/minatures/2.jpg', alt: '' },
-	{ title: 'Dziki źwież',  href: '/mywork/wildlife',  src: 'assets/minatures/3.jpg', alt: '' },
-	{ title: 'Parawany', href: '/mywork/seafaring', src: 'assets/minatures/4.jpg', alt: '' },
-	{ title: 'Dowód że ziemia jest płaska',   href: '/mywork/horizon',   src: 'assets/minatures/5.jpg', alt: '' },
-	{ title: 'Rodzinne strony James\'a Bonda', href: '/mywork/landscape', src: 'assets/minatures/6.jpg', alt: '' },
-	{ title: 'Planeta z Interstellar',  href: '/mywork/crystals',  src: 'assets/minatures/7.jpg', alt: '' },
-	{ title: 'Schody do wody',     href: '/mywork/wharf',     src: 'assets/minatures/8.jpg', alt: '' },
-	{ title: 'Łoweczki',      href: '/mywork/dock',      src: 'assets/minatures/9.jpg', alt: '' }
+	{ title: 'Pagóry', href: '/my-portfolio/mywork/peaks', src: 'assets/minatures/1.jpg', alt: '' },
+	{ title: 'Piaseczki', href: '/my-portfolio/mywork/coast', src: 'assets/minatures/2.jpg', alt: '' },
+	{ title: 'Dziki źwież', href: '/my-portfolio/mywork/wildlife', src: 'assets/minatures/3.jpg', alt: '' },
+	{ title: 'Parawany', href: '/my-portfolio/mywork/seafaring', src: 'assets/minatures/4.jpg', alt: '' },
+	{ title: 'Dowód że ziemia jest płaska', href: '/my-portfolio/mywork/horizon', src: 'assets/minatures/5.jpg', alt: '' },
+	{ title: 'Rodzinne strony James\'a Bonda', href: '/my-portfolio/mywork/landscape', src: 'assets/minatures/6.jpg', alt: '' },
+	{ title: 'Planeta z Interstellar', href: '/my-portfolio/mywork/crystals', src: 'assets/minatures/7.jpg', alt: '' },
+	{ title: 'Schody do wody', href: '/my-portfolio/mywork/wharf', src: 'assets/minatures/8.jpg', alt: '' },
+	{ title: 'Łoweczki', href: '/my-portfolio/mywork/dock', src: 'assets/minatures/9.jpg', alt: '' }
 ]
